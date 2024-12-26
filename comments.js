896858f0be474cbeb7537e46501a5c26b44e63cd
Akshay Kapoor <akshay.kapoor392@gmail.com>
2024-12-26 09:30:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,23 @@
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// Create server
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+// Run the web server
+// Run the server: node comments.js
+// Output: http://localhost:8080/index.html
